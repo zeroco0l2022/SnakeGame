a6601f29fd01b7f0827c37e9a8a2cbd3d78d87bc
zeroco0l <zeroco0l@inbox.ru>
2024-06-05 01:21:53 +0200

    Initial commit

---
+++ Source/SnakeGame/PlayerPawnBase.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerPawnBase.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+APlayerPawnBase::APlayerPawnBase()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	PawnCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PawnCamera"));
+	//RootComponent = PawnCamera;
+	SetRootComponent(PawnCamera);
+}
+
+// Called when the game starts or when spawned
+void APlayerPawnBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerPawnBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerPawnBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
